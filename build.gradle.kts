85af83d6e8dc3ffabfbac5ce580bfe78d2403569
Kieran <kieranipalmer@live.com>
2022-12-01 23:56:59 +0000

    Initial Setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,40 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+val internalDependencies = listOf("refrakt", "akt")
+
 repositories {
+
+    maven {
+        url = uri("https://maven.pkg.github.com/kieranipalmer/refrakt")
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USER")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+        }
+    }
+
+    maven {
+        url = uri("https://maven.pkg.github.com/kieranipalmer/akt")
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USER")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+        }
+    }
+
     mavenCentral()
 }
 
 dependencies {
+
+    //Internal
+    implementation("dev.shanty:refrakt:0.0.1")
+    implementation("dev.shanty:akt:0.0.2")
+
+    //Ktor
+
+    implementation("io.ktor:ktor-server-core-jvm:2.1.3")
+    implementation("io.ktor:ktor-server-netty-jvm:2.1.3")
+
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/dev/shanty/home/server/Application.kt
+++ src/main/kotlin/dev/shanty/home/server/Application.kt
@@ -1,2 +1,16 @@
 package dev.shanty.home.server
 
+import dev.shanty.akt.runLocalActorSystem
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import dev.shanty.refrakt.Lifx
+
+fun main() = runLocalActorSystem {
+
+    val lifx = Lifx(this)
+
+    embeddedServer(Netty) {
+
+    }
+
+}
